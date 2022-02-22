dd43b9d3fa5d33ee37071d6532bdbbe4a300888f
Everth <everth386@hotmail.com>
2022-02-21 22:42:56 -0500

    Menu lateral CV v1.1

--- Js/Cv_Logica.js
+++ Js/Cv_Logica.js
@@ -1,3 +1,19 @@
 'use strict'
 
 
+//Ejecutar funcion el evento click
+document.getElementById("btn_open").addEventListener('click', open_close_menu);
+
+//Declarar variables
+var side_menu = document.getElementById("menu_side");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
+
+
+//Evento para mostrar y ocultar menú
+
+
+function open_close_menu() {
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu_side_move");
+}
