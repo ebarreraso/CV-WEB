b7224c1c1d0d81702158cc349b5560b69f554a60
Everth <everth386@hotmail.com>
2022-02-22 07:04:04 -0500

    Menu lateral CV v1.2

--- Js/Cv_Logica.js
+++ Js/Cv_Logica.js
@@ -17,3 +17,22 @@ function open_close_menu() {
     body.classList.toggle("body_move");
     side_menu.classList.toggle("menu_side_move");
 }
+
+//oculta menu al recargar la pagina si es menor a 760px
+if(window.innerWidth < 760) {
+    body.classList.add("body_move");
+    side_menu.classList.add("menu_side_move");
+}
+
+window.addEventListener("resize", function(){
+
+    if(window.innerHeight > 760){
+        body.classList.remove("body_move");
+        side_menu.classList.remove("menu_side_move");
+    }
+
+    if(window.innerWidth < 760){
+        body.classList.add("body_move");
+        side_menu.classList.add("menu_side_move");
+    }
+});
